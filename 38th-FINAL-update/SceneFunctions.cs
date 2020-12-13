6c4f1e8e5067f62d03a4768fa9bc500685aec3c3
nghi01 <70782465+nghi01@users.noreply.github.com>
2020-12-13 07:17:49 -0500

    Add files via upload

---
+++ 38th-FINAL-update/SceneFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFunctions : MonoBehaviour
+{
+
+  public GameObject QuitButton;
+  public GameObject BackButton;
+  public GameObject StartButton;
+
+  public void QuitGame()
+  {
+    Application.Quit();
+  }
+
+  public void Play()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+  }
+
+  public void Back()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+  }
+}
