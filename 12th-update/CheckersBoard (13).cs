3269b1bbbd79773e36c81d9fca69fc6a78a3dfe2
nghi01 <70782465+nghi01@users.noreply.github.com>
2020-12-13 06:54:34 -0500

    Add files via upload

---
+++ 12th-update/CheckersBoard (13).cs
@@ -0,0 +1,322 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckersBoard : MonoBehaviour
+{
+    public Piece[,] pieces = new Piece[8, 8];
+
+    public GameObject whitePiecePrefab;
+    public GameObject blackPiecePrefab;
+
+    public bool isWhite;
+
+    private Piece selectedPiece;
+
+    private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
+    private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
+
+    private Vector2 mouseOver;
+    private Vector2 starDrag;
+    private Vector2 endDrag;
+
+    private bool isWhiteTurn;
+    private bool hasKilled;
+
+    private List<Piece> forcePieces;
+
+    private void Start()
+    {
+        isWhiteTurn = true;
+        isWhite = true;
+        GenerateBoard();
+        forcePieces = new List<Piece>();
+    }
+
+    private void Update()
+    {
+        UpdateMouseOver();
+
+        //if is my turn
+        IsMyTurn();
+    }
+
+    private void IsMyTurn()
+    {
+        if ((isWhite) ? isWhiteTurn : !isWhiteTurn)
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectPiece(x, y);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                TryMove((int)starDrag.x, (int)starDrag.y, x, y);
+            }
+
+            if (selectedPiece != null)
+            {
+                UpdatePieceDrag(selectedPiece);
+            }
+        }
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        //check if out of bounds
+        if (x < 0 || x >= 8 || y < 0 || y >= 8)
+        {
+            return;
+        }
+
+        Piece p = pieces[x, y];
+        if (p != null && p.isWhite == isWhite)
+        {
+            if (forcePieces.Count == 0)
+            {
+                selectedPiece = p;
+                starDrag = mouseOver;
+            }
+            else
+            {
+                if (forcePieces.Find(fp => fp == p) == null)
+                {
+                    return;
+                }
+                selectedPiece = p;
+                starDrag = mouseOver;
+            }
+        }
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+      //  forcePieces = ScanForPossibleMove();
+
+        starDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+
+        //if out of bounds or has not move
+        if (x2 < 0 || x2 >= pieces.Length || y2 < 0 || y2 >= pieces.Length || endDrag == starDrag)
+        {
+            if (selectedPiece != null)
+                MovePiece(selectedPiece, x1, y1);
+
+            starDrag = Vector2.zero;
+            selectedPiece = null;
+            return;
+        }
+
+        if (selectedPiece != null)
+        {
+            //check valid move
+            if (selectedPiece.ValidMove(pieces, x1, y1, x2, y2))
+            {
+                if (Mathf.Abs(x2 - x1) == 2)
+                {
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null)
+                    {
+                        pieces[(x1 + x2) / 2, (y1 + y2) / 2] = null;
+                        Destroy(p.gameObject);
+                        hasKilled = true;
+                    }
+                }
+
+                if (forcePieces.Count != 0 && !hasKilled)
+                {
+                    ReturnFirstPosition(x1, y1);
+                }
+
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+                MovePiece(selectedPiece, x2, y2);
+                EndTurn();
+            }
+            else
+            {
+                ReturnFirstPosition(x1, y1);
+            }
+        }
+    }
+
+    private void ReturnFirstPosition(int x, int y)
+    {
+        MovePiece(selectedPiece, x, y);
+        starDrag = Vector2.zero;
+        selectedPiece = null;
+        return;
+    }
+
+    private void EndTurn()
+    {
+        int x = (int)endDrag.x;
+        int y = (int)endDrag.y;
+
+        if (selectedPiece != null)
+        {
+            if (selectedPiece.isWhite && !selectedPiece.isKing && y == 7)
+            {
+                selectedPiece.isKing = true;
+                selectedPiece.transform.Rotate(Vector3.right * 180);
+            }
+            else if (!selectedPiece.isWhite && !selectedPiece.isKing && y == 0)
+            {
+                selectedPiece.isKing = true;
+                selectedPiece.transform.Rotate(Vector3.right * 180);
+            }
+        }
+
+        selectedPiece = null;
+        starDrag = Vector2.zero;
+
+      //  if (ScanForPossibleMove(selectedPiece, x, y).Count != 0 && hasKilled)
+      //      return;
+
+        isWhiteTurn = !isWhiteTurn;
+        isWhite = !isWhite;
+        hasKilled = false;
+        CheckVictory();
+    }
+
+    private void CheckVictory()
+    {
+        var ps = FindObjectsOfType<Piece>();
+        bool hasWhite = false, hasBlack = false;
+
+        for (int i = 0; i < ps.Length; i++)
+        {
+            if (ps[i].isWhite)
+                hasWhite = true;
+            else
+                hasBlack = true;
+        }
+
+        if (!hasWhite)
+            Victory(false);
+
+        if (!hasBlack)
+            Victory(true);
+
+    }
+
+    private void Victory(bool iswhite)
+    {
+        if (iswhite)
+            Debug.Log("White team win");
+        else
+            Debug.Log("Black team win");
+    }
+
+    private List<Piece> ScanForPossibleMove(Piece p, int x, int y)
+    {
+        forcePieces = new List<Piece>();
+
+        if (pieces[x, y].IsForceToMove(pieces, x, y))
+            forcePieces.Add(pieces[x, y]);
+
+        return forcePieces;
+    }
+
+    private List<Piece> ScanForPossibleMove()
+    {
+        forcePieces = new List<Piece>();
+
+        //check all the pieces
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (pieces[i, j] != null && pieces[i, j].isWhite == isWhiteTurn)
+                {
+                    if (pieces[i, j].IsForceToMove(pieces, i, j))
+                    {
+                        forcePieces.Add(pieces[i, j]);
+                    }
+                }
+            }
+        }
+
+        return forcePieces;
+    }
+
+    private void UpdateMouseOver()
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
+    }
+
+    private void UpdatePieceDrag(Piece p)
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
+    }
+
+    private void GenerateBoard()
+    {
+        //white pieces
+        for (int y = 0; y < 3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+                GeneratePiece((oddRow) ? x : x + 1, y);
+            }
+        }
+
+        //black pieces
+        for (int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+                GeneratePiece((oddRow) ? x : x + 1, y);
+            }
+        }
+    }
+
+    private void GeneratePiece(int x, int y)
+    {
+        bool isPieceWhite = (y > 3) ? false : true;
+
+        GameObject go = Instantiate((isPieceWhite) ? whitePiecePrefab : blackPiecePrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieces[x, y] = p;
+        MovePiece(p, x, y);
+    }
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
+    }
+}
